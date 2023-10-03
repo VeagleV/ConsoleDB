f2c2a7453caa5b174e82bb704d4f1077c698c765
Eagle <70978893+VeagleV@users.noreply.github.com>
2023-10-03 11:34:54 +0300

    upd main
    добавлена авторизация

--- main.cpp
+++ main.cpp
@@ -1,33 +1,77 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 
-void menuOut(bool admin)
+void menu(bool isAdmin)
 {
-    if(admin==true)
+    if(isAdmin==true)
     {
         printf("//////////////////////////////////////////////////\n");
         printf("// %47s \n","//");
         printf("// %47s \n","//");
         printf("//%27s %24s \n","МЕНЮ","//");
         printf("//%41s %20s\n","1. Вывести таблицу","//");
-        printf("//%53s %14s\n","2. Редактировать таблицу","//");
-        printf("//%49s %16s\n","3. Распечатать таблицу", "//");
-        printf("//%73s %2s\n","4. Вывести таблицу в текстовый файл ", "//");
+        printf("//%53s %14s \n","2. Отсортировать таблицу","//");
+        printf("//%53s %14s\n","3. Редактировать таблицу","//");
+        printf("//%54s %12s\n","4. Вывести таблицу в файл ", "//");
         printf("//%22s %30s\n","5. Выйти","//");
         printf("// %47s \n","//");
         printf("// %47s \n","//");
         printf("//////////////////////////////////////////////////\n");
     } else {
-
+        printf("//////////////////////////////////////////////////\n");
+        printf("// %47s \n","//");
+        printf("// %47s \n","//");
+        printf("//%27s %24s \n","МЕНЮ","//");
+        printf("//%41s %20s\n","1. Вывести таблицу","//");
+        printf("//%53s %14s \n","2. Отсортировать таблицу","//");
+        printf("//%54s %12s\n","3. Вывести таблицу в файл ", "//");
+        printf("//%22s %30s\n","4. Выйти","//");
+        printf("// %47s \n","//");
+        printf("// %47s \n","//");
+        printf("//////////////////////////////////////////////////\n");
 
     }
     
 }
+
+int getMenuChoice(bool isAdmin){
+    int choice;
+
+    cin >> choice;
+    if(isAdmin==true){
+    while(choice>0 && choice<6){
+        
+    }
+    }
+}
+bool login(){
+    bool isAdmin;
+    char login[16];
+    char password[16];
+    char admLogin[16]{"admin"};
+    char admPassword[16]{"admin"};
+    int equal;
+
+    printf("%30s\n","ВХОД В СИСТЕМУ");
+    cout << "\nВведите логин:";
+    cin.getline(login,16);
+    cout << "\nВведите пароль:";
+    cin.getline(password,16);
+    equal = strcmp(login,admLogin) + strcmp(password,admPassword);
+    if (equal == 0){
+        return true;
+    } else {
+        return false;
+    };
+
+
+
+}
 int main(){
     
     setlocale(LC_ALL,"Russian");
-    menuOut(true);
-
+    menu(login());
     return 0;
 }
